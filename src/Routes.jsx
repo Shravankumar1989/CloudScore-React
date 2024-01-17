3aa9c6c0ae8de4bc537978acf6370e94dd192165
Vinodha <biradarvinodha@gmail.com>
2024-01-18 01:43:13 +0800

    Initial Home Page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -7,7 +7,6 @@ const Solutions1439768 = React.lazy(() => import("pages/Solutions1439768"));
 const WhyCS1439768 = React.lazy(() => import("pages/WhyCS1439768"));
 const Blog1439768 = React.lazy(() => import("pages/Blog1439768"));
 const AboutUs1439768 = React.lazy(() => import("pages/AboutUs1439768"));
-const Home1439768 = React.lazy(() => import("pages/Home1439768"));
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
@@ -15,7 +14,6 @@ const ProjectRoutes = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/home1439768" element={<Home1439768 />} />
           <Route path="/aboutus1439768" element={<AboutUs1439768 />} />
           <Route path="/blog1439768" element={<Blog1439768 />} />
           <Route path="/whycs1439768" element={<WhyCS1439768 />} />
